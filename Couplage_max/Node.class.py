206d4fc9c46210422ec3556bc9ac2b141a96e762
Alexandre DERNIAME <kiralex1995@gmail.com>
2018-01-27 14:17:10 +0100

    improve "couplage max" algorithm

--- Couplage_max/Node.class.py
+++ Couplage_max/Node.class.py
@@ -2,7 +2,7 @@
 
 import os
 import time
-from random import randint
+from random import randint, choice
 
 nb_node = -1
 
@@ -16,104 +16,6 @@ class Node(object):
         self.m = False
         self.p = None
         self.neigh = neigh
-
-    """
-    Use to compute pm
-    """
-
-    def pm_compute(self):
-        for v in self.neigh:
-            neigh_j = Node.instances[v]
-            if neigh_j.p == self.id_number and self.p == neigh_j.id_number:
-                return True
-        return False
-
-    # Rule 1
-    def alliance(self):
-        if self.m != self.pm_compute():
-            self.m = self.pm_compute()
-            self.print_state()
-            print("--------------------------------------------\n")
-
-    # Rule 2
-    def wedding(self):
-        if self.m == self.pm_compute() and self.p == None:
-            for jNeigh in self.neigh:
-                neighbour = Node.instances[jNeigh]
-                if neighbour.p == self.id_number:
-                    self.p = neighbour.id_number
-                    self.print_state()
-                    print("--------------------------------------------\n")
-                    return
-
-    # Rule 3
-    def seduction(self):
-        if self.m == self.pm_compute() and self.p is None:
-            for k in self.neigh:
-                neighbour = Node.instances[k]
-                if neighbour.p == self.id_number:
-                    return
-            for j in self.neigh:
-                neighbour = Node.instances[j]
-                if neighbour.p is None and neighbour.id_number > self.id_number and not neighbour.m:
-                    self.p = self.max_seduction()
-                    self.print_state()
-                    print("--------------------------------------------\n")
-                    return
-
-    # Rule 3 bis
-    def max_seduction(self):
-        max = 0
-        for j in self.neigh:
-            neighbour = Node.instances[j]
-            if neighbour.p is None and neighbour.id_number > self.id_number and not neighbour.m:
-                if neighbour.id_number > max:
-                    max = neighbour.id_number
-        return max
-
-    # Rule 4
-    def divorce(self):
-        if self.p is not None:
-            j = Node.instances[self.p]
-            if self.m == self.pm_compute() and self.p is not None and j.p != self.id_number and (
-                j.m or j.id_number <= self.id_number):
-                self.p = None
-                self.print_state()
-                print("--------------------------------------------\n")
-
-    def get_val(self):
-        """get val of a node"""
-        return self.val
-
-    def print_state(self, nb_tab=0):
-        """printState node state"""
-        for i in range(nb_tab):
-            print("\t", end='')
-        print("id: " + str(self.id_number) + ", M: " + str(self.m) + ", P: " + str(self.p) + ", Neigh: " + str(
-            self.neigh))
-
-    def update(self):
-        """
-            update val of a node
-        """
-        continue_update = Node.continue_loop()
-        if continue_update:
-            selected_rule = randint(1, 4)
-            print("node id : " + str(self.id_number) + " | rule id : " + str(selected_rule), end='')
-            if selected_rule == 1:
-                print(" ==> Alliance")
-                self.alliance()
-            elif selected_rule == 2:
-                print(" ==> Wedding")
-                self.wedding()
-            elif selected_rule == 3:
-                print(" ==> Seduction")
-                self.seduction()
-            else:
-                print(" ==> Divorce")
-                self.divorce()
-            print()
-        return continue_update
 
     @staticmethod
     def read_graph_file(file_name, mode):
@@ -145,54 +47,143 @@ class Node(object):
                     Node.instances[b] = node
                     existing_nodes_id.append(b)
                 else:
-                    print(a)
-                    print(b)
                     Node.instances[b].neigh.append(a)
 
-    @staticmethod
-    def continue_loop():
-        """"
-            :return false if nb Node with M properties at "True" is >= nb_node-1
-            :return true otherwise
+    """
+    Use to compute pm
+    """
 
+    def pm_compute(self):
+        for v in self.neigh:
+            neigh_j = Node.instances[v]
+            if neigh_j.p == self.id_number and self.p == neigh_j.id_number:
+                return True
+        return False
+
+    # Rule 1
+    def check_alliance(self):
+        if self.m != self.pm_compute():
+            return True
+        else:
+            return False
+
+    def alliance(self, m):
+        self.m = m
+        self.print_state(0, "Alliance")
+
+    # Rule 2
+    def check_wedding(self):
+        if self.m == self.pm_compute() and self.p == None:
+            for jNeigh in self.neigh:
+                neighbour = Node.instances[jNeigh]
+                if neighbour.p == self.id_number:
+                    return (True, neighbour.id_number)
+
+        return (False, None)
+
+    def wedding(self, id_number):
+        self.p = id_number
+        self.print_state(0, "Mariage")
+
+    # Rule 3
+    def check_seduction(self):
+        if self.m == self.pm_compute() and self.p is None:
+            for k in self.neigh:
+                neighbour = Node.instances[k]
+                if neighbour.p == self.id_number:
+                    return (False, None)
+
+            max = -1
+            for j in self.neigh:
+                neighbour = Node.instances[j]
+                if neighbour.p is None and neighbour.id_number > self.id_number and not neighbour.m:
+                    max = neighbour.id_number
+            if max != -1:
+                return (True, max)
+            else:
+                return (False, None)
+
+        return (False, None)
+
+    def seduction(self, id_number):
+        self.p = id_number
+        self.print_state(0, "Séduction")
+
+    # Rule 4
+    def check_divorse(self):
+        if self.p is not None:
+            j = Node.instances[self.p]
+            if self.m == self.pm_compute() and self.p is not None and j.p != self.id_number and (
+                    j.m or j.id_number <= self.id_number):
+                return True
+
+        return False
+
+    def divorce(self):
+        self.p = None
+        self.print_state(0, "Divorce")
+
+    def print_state(self, nb_tab=0, typeOperation=""):
+        """printState node state"""
+        for i in range(nb_tab):
+            print("\t", end='')
+        print("id: " + str(self.id_number) + ", M: " + str(self.m) + ", P: " + str(self.p) + ", Neigh: " + str(
+            self.neigh) + "\t" + typeOperation)
+
+    def update(self):
         """
-        count = 0
-        for node_v in Node.instances.values():
-            if node_v.m:
-                count += 1
-        if nb_node%2 == 0: # even number
-            res = (count < len (Node.instances))
-        else: # not even number
-            res = (count < len(Node.instances) - 1)
-        return res
+            update val of a node
+        """
+
+        if self.check_alliance():
+            self.alliance(True)
+            return True
+
+        (res, node_id) = self.check_wedding()
+        if res:
+            self.wedding(node_id)
+            return True
+
+        yolo = self.check_seduction()
+        print(yolo)
+        (res, node_id) = yolo
+
+        if res:
+            self.seduction(node_id)
+            return True
+
+        if self.check_divorse():
+            self.divorce()
+            return True
+
+        return False
+
+
+def initlist():
+    list_nodes = []
+    for node in Node.instances.values():
+        list_nodes.append(node)
+    return list_nodes
 
 
 if __name__ == '__main__':
-    continue_loop = True
-    count_loop = 0
     dir_path = os.path.dirname(os.path.realpath(__file__))
     # Read and make graph structure
     Node.read_graph_file(dir_path + '/graphe_config.txt', 'r')
 
-    print("===== Graph Begin configuration ====================")
+    print("============ Initial configurations ============")
     for node in Node.instances.values():
         node.print_state()
-    print("==============================================\n")
+    print("================================================")
 
-    print("---- Execution -------------------------------\n")
-    while continue_loop and count_loop < 10000:
-        if count_loop%1000 == 0:
-            print("===== Graph " + str(count_loop) + " iterate configuration ====================")
-            for node in Node.instances.values():
-                node.print_state()
-            print("==============================================\n")
+    list_nodes = initlist()
+    while len(list_nodes) > 0:
+        node = choice(list_nodes)
+        res = node.update()
+        if res:
+            list_nodes = initlist()
+        else:
+            list_nodes.remove(node)
 
-        ID = randint(1, nb_node)
-        continue_loop = Node.instances[ID].update()
-        count_loop += 1
-        # time.sleep(0.005)
-
-
-    print("===== Graph Final configuration ====================")
     for node in Node.instances.values():
         node.print_state()
